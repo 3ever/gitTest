3d5adbd33159b7905905745477baa1cbf84469aa
flowerDance <flowe@Dance.com>
2022-09-12 17:55:47 +0800

    second commit

--- gitTest/src/com/maofugui/git/gitTest.java
+++ gitTest/src/com/maofugui/git/gitTest.java
@@ -3,5 +3,6 @@ package com.maofugui.git;
 public class gitTest {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
