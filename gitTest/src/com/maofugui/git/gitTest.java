96c966dc4d5f8ae98b98a6bf485ec2bda3d19c43
flowerDance <flowe@Dance.com>
2022-09-12 18:19:24 +0800

    hot-fix commit1

--- gitTest/src/com/maofugui/git/gitTest.java
+++ gitTest/src/com/maofugui/git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("Hello World");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
+        System.out.println("Hello World4--hot-fix");
     }
 }
