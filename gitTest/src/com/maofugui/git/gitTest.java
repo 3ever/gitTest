3ed5405605b4dfc1aee12315b0c92bca128b0cdc
3ever <50514089+3ever@users.noreply.github.com>
2022-09-12 23:41:27 +0800

    pull commit

--- gitTest/src/com/maofugui/git/gitTest.java
+++ gitTest/src/com/maofugui/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("Hello World4--hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix  test");
+        System.out.println("pull commit");
     }
 }
